59c71fdd798661fe4562772eafabfc7478308a63
Takeshi Stormer <takeshi.stormer@gmail.com>
2025-11-16 14:39:16 -0800

    Brushed up on some of the formatting and included brief documentation.

--- Project/scripts/data_cleaning_na.R
+++ Project/scripts/data_cleaning_na.R
@@ -1,21 +1,38 @@
+# Project/scripts/data_cleaning_na.R
+# ------------------------------------------------------------------
+#  PROJECT DATA CLEANER (NA)
+# ------------------------------------------------------------------
+# Purpose:
+#   - Document NA values within the data file.
+# ------------------------------------------------------------------
+
+# ---------------------------
+# Install Dependencies
+# ---------------------------
 library(tidyverse)
 library(arrow)
 
+# ------------------------------------------------------------------
+# Install base unmodified bid data as ad_data.
+# ------------------------------------------------------------------
+base_data_path = file.path(getwd(), "Project/scripts/load_data.R")
+source(base_data_path)
 
-df <- read_parquet("Project/data/bids_data_vDTR.parquet")
+# ------------------------------------------------------------------
+#  NOTED NA VALUES (KAN-14)
+# ------------------------------------------------------------------
+# List of KEYS with respective NA value count.
+na_by_col <- colSums(is.na(ad_data))
 
-
-na_by_col <- colSums(is.na(df))
-na_by_col
-
+# List of KEYS that have NA values.
 cols_with_na <- names(na_by_col[na_by_col > 0])
-cols_with_na
 
-rows_with_na <- df %>% filter(if_any(everything(), is.na))
+# Returns tibble of rows that contain NA values.
+rows_with_na <- ad_data %>% filter(if_any(everything(), is.na))
 nrow(rows_with_na)
 
-# Percentage of missing values by column
-na_pct <- (na_by_col / nrow(df)) * 100
-na_pct
+# Percentage of NA values by KEY
+na_pct <- (na_by_col / nrow(ad_data)) * 100
 
+# Percentage of NA values by KEYS that have NA values. 
 na_pct[c("DEVICE_GEO_CITY", "DEVICE_GEO_ZIP")]
