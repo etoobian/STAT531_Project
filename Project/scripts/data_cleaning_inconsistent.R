edcfbc62bf912abba1b11ffa84e09c7880480a8c
Takeshi Stormer <takeshi.stormer@gmail.com>
2025-11-15 20:54:05 -0800

    Formatted file for documentation.

--- Project/scripts/data_cleaning_inconsistent.R
+++ Project/scripts/data_cleaning_inconsistent.R
@@ -1,1 +1,25 @@
 # Project/scripts/data_cleaning_inconsistent.R
+# ------------------------------------------------------------------
+#  PROJECT DATA CLEANER (inconsistencies)
+# ------------------------------------------------------------------
+# Purpose:
+#   - Document noticeable errors/inconsistency with data types, formatting, prior data modifications (some of the 12 bombs), and spelling mistakes to name a few.
+#
+# NOTES:
+#   - May otherwise miss some errors/inconsistencies.
+# ------------------------------------------------------------------
+
+# ------------------------------------------------------------------
+#  NOTED ERRORS/INCONSITENCIES
+# ------------------------------------------------------------------
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
