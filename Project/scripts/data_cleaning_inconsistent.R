f3c5a5855b9ba23747f1525109af4fe86a8119c6
Takeshi Stormer <takeshi.stormer@gmail.com>
2025-11-16 09:49:17 -0800

    Final batch of errors/inconsistencies.

--- Project/scripts/data_cleaning_inconsistent.R
+++ Project/scripts/data_cleaning_inconsistent.R
@@ -25,8 +25,9 @@
 #   - TIMESTAMP <chr>: Data set is not sorted chronologically.
 #   - DEVICE_GEO_LONG <dbl>: There exists exactly 100 points that lie beyond the border of Oregon.
 #   + print(filter(.data=ad_data, DEVICE_GEO_LONG < -130), n=20)
+#   + These points lie exactly 10 degrees to the west of corresponding city longitudinal values.
 #   - PRICE <chr>: There exist prices that are negative.
-#   - PRICE <chr>: There exist negative prices that have BID_WON == TRUE.
+#   - PRICE <chr>: There exist negative prices that have BID_WON==TRUE.
 #   - PRICE <chr>: There exist prices that are close to -1000 (-999).
 #   - BID_WON <chr>: Should be of data type logical.
 #   - AUCTION_ID, BID_WON <chr, chr>: There are bids with multiple wins.
@@ -43,9 +44,13 @@
 #   + The last three-four characters should contain the desired information.
 #   - RESPONSE_TIME <chr>: Inconsistent formatting.
 #   + There exists at least one RESPONSE_TIME value that has the following delimiter RESSPONSE_TIME (as opposed to RESPONSE_TIME).
+#   - REQUESTED_SIZE, SIZE <chr, chr>: There exists requested sizes of 0x0 and 1x1 that also do not meet requested size specifications despite have BID_WON==TRUE
+#   + We would not be able to determine the actual size won for these invalid SIZE values if REQUESTED_SIZE contains more than 2 requested sizes. 
 #
 #  Tested for:
 #   - Multiple time entries for distinct AUCTION_ID.
 #   - Pattern in inconsistent formatting for TIMESTAMP
 #   + Could be an issue with it being not in chronological order (error applied before scrambling).
-#   - 
+#   - Noted that some cities encompass more than 20 zip codes in DEVICE_GEO_ZIP and DEVICE_GEO_ZIP
+#   - Noted that at least some AUCTION_ID values are encompassed purely by one bider in PUBLISHER_ID (one bider bidding multiple times).
+#   + For further exploration, we can potentially re-order these instances in chronological order potentially by ordering by RESPONSE_TIME
