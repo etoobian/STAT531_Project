365fbf9efea7553f6f6948cfb78df9d50079a7ec
Takeshi Stormer <takeshi.stormer@gmail.com>
2025-11-15 22:45:03 -0800

    Identified first batch of errors/inconsistencies.

--- Project/scripts/data_cleaning_inconsistent.R
+++ Project/scripts/data_cleaning_inconsistent.R
@@ -12,14 +12,38 @@
 # ------------------------------------------------------------------
 #  NOTED ERRORS/INCONSITENCIES
 # ------------------------------------------------------------------
+#   
+#  Template:
+#   - [KEY(S)] <DATATYPE(S)>: Error
+#   + [Code if applicable]
+#   + [Note if applicable]
 #
+#  Errors/Inconsistencies:
+#   - PRICE <chr>: Should be of data type double.
+#   - DEVICE_GEO_ZIP <chr>: Should be of data type integer.
+#   - TIMESTAMP <chr>: Inconsistent formatting.
+#   - TIMESTAMP <chr>: Data set is not sorted chronologically.
+#   - DEVICE_GEO_LONG <dbl>: There exists exactly 100 points that lie beyond the border of Oregon.
+#   + print(filter(.data=ad_data, DEVICE_GEO_LONG < -130), n=20)
+#   - PRICE <chr>: There exist prices that are negative.
+#   - PRICE <chr>: There exist negative prices that have BID_WON == TRUE.
+#   - PRICE <chr>: There exist prices that are close to -1000 (-999).
+#   - BID_WON <chr>: Should be of data type logical.
+#   - AUCTION_ID, BID_WON <chr, chr>: There are bids with multiple wins.
+#   + filter(.data=summarise(.data=ad_data, won=sum(BID_WON=="TRUE"), lost=sum(BID_WON=="FALSE"), total=n()), won > 1)
+#   + The existence of an auction have 6 winners can indicate that no shifting of columns occurred.
+#   - TIMESTAMP, DATE_UTC <chr, chr>: DATE_UTC does not reflect days properly according to timezone differences.
+#   + Helpful since DATE_UTC reflects PST, which we can utilize to correctly index existing TIMESTAMP values.
+#   - DEVICE_GEO_REGION <chr>: Inconsistent formatting.
+#   - DEVICE_GEO_CITY <chr>: Existence of non-cities (unincorporated communities, CDPs, etc.).
+#   + print(unique(ad_data$DEVICE_GEO_CITY), n=100, na.print=".")
+#   + Possibly negligible.
+#   - DEVICE_GEO_ZIP <chr>: There exists a negative ZIP code (-999).
+#   - RESPONSE_TIME <chr>: Should be of type integer.
+#   + The last three-four characters should contain the desired information.
 #
-#
-#
-#
-#
-#
-#
-#
-#
-#
+#  Tested for:
+#   - Multiple time entries for distinct AUCTION_ID.
+#   - Pattern in inconsistent formatting for TIMESTAMP
+#   + Could be an issue with it being not in chronological order (error applied before scrambling).
+#   - 
